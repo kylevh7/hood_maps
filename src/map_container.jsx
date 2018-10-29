efeea1030c66df15e3eb7eb0d3abc2c0bf7ab745
unknown <kylevh7@gmail.com>
2018-10-29 17:40:28 -0400

    Have markers generating on the screen from foursquare

--- src/map_container.jsx
+++ src/map_container.jsx
@@ -1,50 +1,58 @@
 import React, {Component} from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps";
 import axios from 'axios';
 
+const MyMapComponent = withScriptjs(withGoogleMap((props) => <GoogleMap zoom={props.zoom} center={props.center}>
+    {
+        props.markers && props.markers.filter(marker => marker.isVisable).map((marker, index) => <Marker key={index} position={{
+                lat: marker.lat,
+                lng: marker.lng
+            }}/>)
+    }
+</GoogleMap>))
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 44, lng: -85 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 44, lng: -85}} />}
-  </GoogleMap>
-))
+class MapContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            venues: [],
+            center: [],
+            markers: [],
+            zoom: 11
+        }
+    }
 
-
-class MapContainer extends Component{
-    componentDidMount(){
+    componentDidMount() {
         this.getPlaces()
     }
 
-    getPlaces=()=>{
+    getPlaces = () => {
         const endPoint = "https://api.foursquare.com/v2/venues/search?";
-        const params={
-        client_id:"G15TPMV3XGKPDH0QEY4BMFMZMRJAQ4CUTIKL0KXAPQXPVA5I",
-        client_secret:"OP1FJ5MLN22TKJWGYZDQEXA51A0APPSUFNCAU2S0WNEBD2ZG",
+        const params = {
+            client_id: "G15TPMV3XGKPDH0QEY4BMFMZMRJAQ4CUTIKL0KXAPQXPVA5I",
+            client_secret: "OP1FJ5MLN22TKJWGYZDQEXA51A0APPSUFNCAU2S0WNEBD2ZG",
             query: "coffee",
-            near: "McBain, MI",
+            near: "Cadillac, MI",
             limit: 10,
             v: "20181027"
         }
-    axios.get(endPoint + new URLSearchParams(params))
-        .then(response=>{
+        axios.get(endPoint + new URLSearchParams(params)).then(response => {
             console.log(response)
-        })
-        .catch(err=>{
-            console.log("UghOhhh"+ err)
+            const venues = response.data.response.venues;
+            const center = response.data.response.geocode.feature.geometry.center;
+            const markers = venues.map(venue => {
+                return {lat: venue.location.lat, lng: venue.location.lng, isOpen: false, isVisable: true}
+            })
+            this.setState({venues, center, markers});
+            console.log(markers)
+
+        }).catch(err => {
+            console.log("UghOhhh" + err)
         })
     }
 
-render(){
-    return(<MyMapComponent
-      isMarkerShown
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />)
-};
+    render() {
+        return (<MyMapComponent zoom={this.state.zoom} center={this.state.center} markers={this.state.markers} isMarkerShown="isMarkerShown" googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `400px` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>)
+    };
 };
 export default MapContainer;
