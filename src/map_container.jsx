704432649ec23ba3ed91c2bb12b38fef270f20d9
unknown <kylevh7@gmail.com>
2018-11-03 09:30:56 -0400

    photos popping up when clicking marker

--- src/map_container.jsx
+++ src/map_container.jsx
@@ -3,19 +3,22 @@ import {withScriptjs, InfoWindow, withGoogleMap, GoogleMap, Marker} from "react-
 import axios from 'axios';
 import Search from "./Search"
 
+
 const MyMapComponent = withScriptjs(withGoogleMap((props) => <GoogleMap zoom={props.zoom} center={props.center}>
 
+
+
     {
-        props.markers && props.markers.filter(marker => marker.isVisable).map((marker, index) =>
+        props.markers && props.markers.filter(marker => marker.isVisable).map((marker,index) =>
          <Marker key={index} position={{
                 lat: marker.lat,
                 lng: marker.lng
-            }} onClick={() => props.infoWindow(marker)} onClick={()=> props.getPhotos(marker)}>
+            }} onClick={() => {props.infoWindow(marker)}}>
             {
                 marker.isOpen && (<InfoWindow>
                     <div>
-                        <img src={`${marker.photoPrefix}200x200${marker.photoSuffix}`} alt={"image of " + marker.name}/>
-                        <p>"hi"</p>
+                        <img src={`${props.photos.prefix}200x200${props.photos.suffix}`} alt={"image of " + marker.name}/>
+                        <p></p>
                     </div>
                 </InfoWindow>)
             }
@@ -31,6 +34,7 @@ class MapContainer extends Component {
             venues: [],
             center: [],
             markers: [],
+            photos:{},
             zoom: 11
         }
     }
@@ -38,6 +42,8 @@ class MapContainer extends Component {
     componentDidMount() {
         this.getPlaces()
     }
+
+
 
     closeWindow = () => {
         const marker = this.state.markers.map(marker => {
@@ -53,26 +59,34 @@ class MapContainer extends Component {
         this.closeWindow();
         marker.isOpen = true;
         this.setState({
-            markers: Object.assign(this.state.markers, marker)
-        })
-    }
-    getPhotos = (marker) => {
+            markers: Object.assign(this.state.markers,marker)
+        });
+
+
+    (() => {
         const id=marker.id
         console.log(id);
         const photoSearch = "https://api.foursquare.com/v2/venues/"+id+"/photos?"
         const photoParams = {
             client_id: "G15TPMV3XGKPDH0QEY4BMFMZMRJAQ4CUTIKL0KXAPQXPVA5I",
             client_secret: "OP1FJ5MLN22TKJWGYZDQEXA51A0APPSUFNCAU2S0WNEBD2ZG",
-            VENUE_ID:id,
             limit: 10,
             v: "20181027"
         }
         axios.get(photoSearch + new URLSearchParams(photoParams)).then(res=>{
-            console.log(res);
+            const prefix=res.data.response.photos.items[0].prefix
+            const suffix=res.data.response.photos.items[0].suffix
+            const photos={
+                prefix:prefix,
+                suffix:suffix
+            }
+            this.setState({photos})
+            console.log(res)
         }).catch(err=>{
             console.log(err)
         })
-    }
+    })()
+}
 
     getPlaces = () => {
         const localSearch = "https://api.foursquare.com/v2/venues/search?";
@@ -93,7 +107,6 @@ class MapContainer extends Component {
             return {lat: venue.location.lat, lng: venue.location.lng, isOpen: false, isVisable: true, name: venue.name, id:venue.id}
         })
         this.setState({venues, center, markers});
-console.log(this.state.markers);
     }).catch(err => {
         console.log(err)
     })
@@ -102,7 +115,8 @@ console.log(this.state.markers);
 
 
     render() {
-        return (<MyMapComponent zoom={this.state.zoom} center={this.state.center} markers={this.state.markers} infoWindow={this.infoWindow} getPhotos={this.getPhotos} isMarkerShown="isMarkerShown" googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD-ceCUOPuTZyturppPcZaHKA9REttJa-0" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `80vh` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>)
+        return (<MyMapComponent zoom={this.state.zoom} center={this.state.center} markers={this.state.markers} infoWindow={this.infoWindow} photos={this.state.photos}  isMarkerShown="isMarkerShown"
+        combineFunctions={this.combineFunctions} googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD-ceCUOPuTZyturppPcZaHKA9REttJa-0" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `80vh` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>)
     };
 };
 export default MapContainer;
