464ed9c4739a83e44897c2c6c171f8873682776d
unknown <kylevh7@gmail.com>
2018-11-03 23:16:17 -0400

    rendering sidebar in map_container with fragment

--- src/map_container.jsx
+++ src/map_container.jsx
@@ -3,18 +3,15 @@ import {withScriptjs, InfoWindow, withGoogleMap, GoogleMap, Marker} from "react-
 import axios from 'axios';
 import Sidebar from './sideBar'
 
-
-
 const MyMapComponent = withScriptjs(withGoogleMap((props) => <GoogleMap zoom={props.zoom} center={props.center}>
 
-
-
     {
-        props.markers && props.markers.filter(marker => marker.isVisable).map((marker,index) =>
-         <Marker key={index} position={{
+        props.markers && props.markers.filter(marker => marker.isVisable).map((marker, index) => <Marker key={index} position={{
                 lat: marker.lat,
                 lng: marker.lng
-            }} onClick={() => {props.infoWindow(marker)}}>
+            }} onClick={() => {
+                props.infoWindow(marker)
+            }}>
             {
                 marker.isOpen && (<InfoWindow>
                     <div>
@@ -36,7 +33,7 @@ class MapContainer extends Component {
             venues: [],
             center: [],
             markers: [],
-            photos:{},
+            photos: {},
             zoom: 11
         }
     }
@@ -44,8 +41,6 @@ class MapContainer extends Component {
     componentDidMount() {
         this.getPlaces()
     }
-
-
 
     closeWindow = () => {
         const marker = this.state.markers.map(marker => {
@@ -61,33 +56,33 @@ class MapContainer extends Component {
         this.closeWindow();
         marker.isOpen = true;
         this.setState({
-            markers: Object.assign(this.state.markers,marker)
+            markers: Object.assign(this.state.markers, marker)
         });
 
-(()=>{
-        const id=marker.id
-        console.log(id);
-        const photoSearch = "https://api.foursquare.com/v2/venues/"+id+"/photos?"
-        const photoParams = {
-            client_id: "G15TPMV3XGKPDH0QEY4BMFMZMRJAQ4CUTIKL0KXAPQXPVA5I",
-            client_secret: "OP1FJ5MLN22TKJWGYZDQEXA51A0APPSUFNCAU2S0WNEBD2ZG",
-            limit: 10,
-            v: "20181027"
-        }
-        axios.get(photoSearch + new URLSearchParams(photoParams)).then(res=>{
-            const prefix=res.data.response.photos.items[0].prefix
-            const suffix=res.data.response.photos.items[0].suffix
-            const photos={
-                prefix:prefix,
-                suffix:suffix
+        (() => {
+            const id = marker.id
+            console.log(id);
+            const photoSearch = "https://api.foursquare.com/v2/venues/" + id + "/photos?"
+            const photoParams = {
+                client_id: "G15TPMV3XGKPDH0QEY4BMFMZMRJAQ4CUTIKL0KXAPQXPVA5I",
+                client_secret: "OP1FJ5MLN22TKJWGYZDQEXA51A0APPSUFNCAU2S0WNEBD2ZG",
+                limit: 10,
+                v: "20181027"
             }
-            this.setState({photos})
-            console.log(res)
-        }).catch(err=>{
-            console.log(err)
-        })
-    })()
-}
+            axios.get(photoSearch + new URLSearchParams(photoParams)).then(res => {
+                const prefix = res.data.response.photos.items[0].prefix
+                const suffix = res.data.response.photos.items[0].suffix
+                const photos = {
+                    prefix: prefix,
+                    suffix: suffix
+                }
+                this.setState({photos})
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        })()
+    }
 
     getPlaces = () => {
         const localSearch = "https://api.foursquare.com/v2/venues/search?";
@@ -100,27 +95,35 @@ class MapContainer extends Component {
             v: "20181027"
         }
 
-    axios.get(localSearch + new URLSearchParams(searchParams)).then(response => {
-        console.log(response)
-        const venues = response.data.response.venues;
-        const center = response.data.response.geocode.feature.geometry.center;
-        const markers = venues.map(venue => {
-            return {lat: venue.location.lat, lng: venue.location.lng, isOpen: false, isVisable: true, name: venue.name, id:venue.id}
+        axios.get(localSearch + new URLSearchParams(searchParams)).then(response => {
+            console.log(response)
+            const venues = response.data.response.venues;
+            const center = response.data.response.geocode.feature.geometry.center;
+            const markers = venues.map(venue => {
+                return {
+                    lat: venue.location.lat,
+                    lng: venue.location.lng,
+                    isOpen: false,
+                    isVisable: true,
+                    name: venue.name,
+                    id: venue.id
+                }
+            })
+            this.setState({venues, center, markers});
+        }).catch(err => {
+            console.log(err)
         })
-        this.setState({venues, center, markers});
-    }).catch(err => {
-        console.log(err)
-    })
-}
-
-
+    }
 
     render() {
-        return (<MyMapComponent zoom={this.state.zoom} center={this.state.center} markers={this.state.markers} infoWindow={this.infoWindow} photos={this.state.photos}  isMarkerShown="isMarkerShown"
-        combineFunctions={this.combineFunctions} googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD-ceCUOPuTZyturppPcZaHKA9REttJa-0" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `100vh`, width:`100vw` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>
-
+        return (
+            <React.Fragment>
+            <Sidebar />
+        <MyMapComponent zoom={this.state.zoom} center={this.state.center} markers={this.state.markers} infoWindow={this.infoWindow} photos={this.state.photos} isMarkerShown="isMarkerShown" combineFunctions={this.combineFunctions} googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyD-ceCUOPuTZyturppPcZaHKA9REttJa-0" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `100vh`, width:`100vw` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>
+        </React.Fragment>
     )
     };
 
 };
+
 export default MapContainer;
